cb306d492194893c8f6c0eec43bc8b72f67cd425
JinjuNa <jinju@liveklass.com>
2024-09-07 18:16:32 +0900

    💄 style: Change logo image and header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
-import { GitlabIcon, UserIcon } from "lucide-react";
+import { Github, UserIcon } from "lucide-react";
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between bg-background px-4 py-3 shadow-sm sm:px-6">
+    <header className="flex items-center justify-between bg-background px-1 py-1 shadow-sm sm:px-6 bg-[#ffc107]">
       <Link href="#" className="flex items-center gap-2" prefetch={false}>
-        <UserIcon className="h-6 w-6" />
+        <Image
+          src="/images/ui-helper-logo.png"
+          alt="UI Helper Logo"
+          width={50}
+          height={50}
+        />
         <span className="text-lg font-medium">UI Helper</span>
       </Link>
       <Link
@@ -14,7 +20,7 @@ const Header = () => {
         className="text-muted-foreground hover:text-foreground"
         prefetch={false}
       >
-        <GitlabIcon className="h-6 w-6" />
+        <Github className="h-6 w-6" />
         <span className="sr-only">GitHub</span>
       </Link>
     </header>
