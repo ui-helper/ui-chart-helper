05ab2f288dcb3bc1dbfdcd1796f02df4673dbef6
suhyeoonn <suhyeon1740@gmail.com>
2024-09-07 23:07:13 +0900

    🐛 bug: Move copy handler from icon to button

--- src/components/CodeViewer.tsx
+++ src/components/CodeViewer.tsx
@@ -30,12 +30,9 @@ const CodeViewer = () => {
             variant="ghost"
             size="sm"
             className="absolute top-0 z-10 right-0 m-2 shadow-md bg-[#ffc107] hover:bg-[#c89706]"
+            onClick={() => copyText(codeString)}
           >
-            {copied ? (
-              <Check size={16} />
-            ) : (
-              <ClipboardIcon size={16} onClick={() => copyText(codeString)} />
-            )}
+            {copied ? <Check size={16} /> : <ClipboardIcon size={16} />}
           </Button>
 
           <SyntaxHighlighter language="jsx" style={dark} className="rounded-md">
