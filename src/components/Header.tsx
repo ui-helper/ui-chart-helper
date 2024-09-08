bc444eb3503cdc03cfe2441f36f04208708d66e5
jinju <dindoo0524@gmail.com>
2024-09-08 21:59:29 +0900

    💚 chore: Remove image component temporarily

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,11 @@
-import { Github, UserIcon } from "lucide-react";
+import { Github } from "lucide-react";
 import Link from "next/link";
 import React from "react";
-import Image from "next/image";
 
 const Header = () => {
   return (
     <header className="flex items-center justify-between px-1 py-1 shadow-sm sm:px-6 bg-[#ffc107]">
       <Link href="#" className="flex items-center gap-2" prefetch={false}>
-        <Image
-          src="/images/ui-helper-logo.png"
-          alt="UI Helper Logo"
-          width={50}
-          height={50}
-        />
         <span className="text-lg font-medium">UI Helper</span>
       </Link>
       <Link
